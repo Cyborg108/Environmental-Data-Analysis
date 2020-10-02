fa2c728903858547e27e85a9ac605d6598a2f3d7
Rupert Li <rupertlihero@gmail.com>
2020-10-02 09:07:46 -0700

    Create runcommand.sh

---
+++ FireData/runcommand.sh
@@ -0,0 +1,9 @@
+./process_data AER_OD MODAL2_M_AER_OD > t_2017_61zone_AER_OD.txt  
+./process_data AER_RA MODAL2_M_AER_RA > t_2017_61zone_AER_RA.txt
+./process_data CO MOP_CO_M > t_2017_61zone_CO.txt
+./process_data ozone AURA_OZONE_M > t_2017_61zone_OZONE.txt
+./process_data Fire MOD14A1_M_FIRE > t_2017_61zone_Fire.txt
+./process_data NO2 AURA_NO2_M > t_2017_61zone_NO2.txt
+
+fuoco.geog.umd.edu/modis/process_FRP 
+//Tropospheric_ozone/process_tco
